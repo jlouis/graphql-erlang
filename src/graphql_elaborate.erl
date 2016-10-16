9343500c6d1adceb1d35fa9314ad1787b468d5d6
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2016-10-16 12:34:10 +0200

    Remove the x_ prefix inside the elaborator.
    We already know we are trying to elaborate when we are inside the module.
    There is no reason to say so again and again. so remove the x_ prefix, hopefully
    making the code a bit more readable in the process.

--- src/graphql_elaborate.erl
+++ src/graphql_elaborate.erl
@@ -6,41 +6,41 @@
 -export([x/1]).
 
 -spec x(graphql:ast()) -> graphql:ast().
-x(Doc) -> x_document(Doc).
+x(Doc) -> document(Doc).
 
-x_document({document, Ops}) ->
-    {document, x_ops([document], Ops)}.
+document({document, Ops}) ->
+    {document, ops([document], Ops)}.
 
-x_ops(_Path, []) -> [];
-x_ops(Path, [#frag {} = F | OPs]) ->
-    [x_frag([F | Path], F) | x_ops(Path, OPs)];
-x_ops(Path, [#op{} = O | OPs]) ->
-    [x_op([O | Path], O) | x_ops(Path, OPs)].
+ops(_Path, []) -> [];
+ops(Path, [#frag {} = F | OPs]) ->
+    [frag([F | Path], F) | ops(Path, OPs)];
+ops(Path, [#op{} = O | OPs]) ->
+    [op([O | Path], O) | ops(Path, OPs)].
 
 %% -- FRAGMENTS -----------------------------------
-x_frag(Path, #frag { ty = T } = F) ->
+frag(Path, #frag { ty = T } = F) ->
    Ty = name(T),
    case graphql_schema:lookup(Ty) of
        not_found ->
            graphql_err:abort([F | Path], {type_not_found, Ty});
        #object_type{ fields = Fields } = Obj ->
-           x_fields([F | Path], F#frag { schema = Obj }, Fields);
+           fields([F | Path], F#frag { schema = Obj }, Fields);
        #interface_type{ fields = Fields } = IFace ->
-           x_fields([F | Path], F#frag { schema = IFace }, Fields);
+           fields([F | Path], F#frag { schema = IFace }, Fields);
        #union_type {} = Union ->
            %% A union can only be elaborated on an empty field set:
-           x_fields([F | Path], F#frag { schema = Union }, #{})
+           fields([F | Path], F#frag { schema = Union }, #{})
    end.
 
 %% -- OPERATIONS -----------------------------------
 
-x_op(Path, #op { vardefs = VDefs } = Op) ->
-    RootSchema = x_root(Path, Op),
+op(Path, #op { vardefs = VDefs } = Op) ->
+    RootSchema = root(Path, Op),
     case graphql_schema:lookup(RootSchema) of
         not_found ->
             graphql_err:abort([Op | Path], {type_not_found, RootSchema});
         #object_type{ fields = Fields } = Obj ->
-            x_fields(Path, Op#op{ schema = Obj, vardefs = vdefs([Op | Path], VDefs) }, Fields)
+            fields(Path, Op#op{ schema = Obj, vardefs = vdefs([Op | Path], VDefs) }, Fields)
     end.
 
 vdef_lookup(Path, N) ->
@@ -69,7 +69,7 @@ unwrap_type({name, N, _}) -> N;
 unwrap_type({non_null, Ty}) -> unwrap_type(Ty);
 unwrap_type({list, Ty}) -> unwrap_type(Ty).
 
-x_root(Path, #op { ty = T } = Op) ->
+root(Path, #op { ty = T } = Op) ->
     case graphql_schema:lookup('ROOT') of
         #root_schema { query = Q, mutation = M, subscription = S } ->
             graphql_ast:resolve_root_type(T, Q, M, S);
@@ -79,18 +79,18 @@ x_root(Path, #op { ty = T } = Op) ->
 
 %% -- SELECTION SETS -------------------------------
 
-x_fields(Path, #frag { selection_set = SSet} = F, Fields) ->
-    F#frag{ selection_set = x_sset(Path, SSet, Fields)};
-x_fields(Path, #op{ selection_set = SSet} = O, Fields) ->
-    O#op{ selection_set = x_sset(Path, SSet, Fields)}.
+fields(Path, #frag { selection_set = SSet} = F, Fields) ->
+    F#frag{ selection_set = sset(Path, SSet, Fields)};
+fields(Path, #op{ selection_set = SSet} = O, Fields) ->
+    O#op{ selection_set = sset(Path, SSet, Fields)}.
 
-x_sset(Path, SSet, Fields) ->
-    [x_field(Path, S, Fields) || S <- SSet].
+sset(Path, SSet, Fields) ->
+    [field(Path, S, Fields) || S <- SSet].
     
-x_field(_Path, #frag_spread {} = FragSpread, _Fields) -> FragSpread;
+field(_Path, #frag_spread {} = FragSpread, _Fields) -> FragSpread;
 %% Inline fragments are elaborated the same way as fragments
-x_field(Path, #frag { id = '...' } = Frag, _Fields) -> x_frag(Path, Frag);
-x_field(Path, #field { id = ID, args = Args, selection_set = SSet } = F, Fields) ->
+field(Path, #frag { id = '...' } = Frag, _Fields) -> frag(Path, Frag);
+field(Path, #field { id = ID, args = Args, selection_set = SSet } = F, Fields) ->
     Name = name(ID),
     case maps:get(Name, Fields, not_found) of
         not_found when Name == <<"__typename">> ->
@@ -101,23 +101,23 @@ x_field(Path, #field { id = ID, args = Args, selection_set = SSet } = F, Fields)
             case graphql_ast:unwrap_type(Ty) of
                 {scalar, _} ->
                     F#field {
-                      args = x_field_args([F | Path], Args, SArgs),
+                      args = field_args([F | Path], Args, SArgs),
                       schema = SchemaTy,
                       schema_obj = scalar };
                 T when is_binary(T) ->
-                    {Obj, SSet2} = x_field_lookup([F | Path], T, SSet),
+                    {Obj, SSet2} = field_lookup([F | Path], T, SSet),
                     F#field {
-                      args = x_field_args([F | Path], Args, SArgs),
+                      args = field_args([F | Path], Args, SArgs),
                       schema = SchemaTy,
                       schema_obj = Obj,
                       selection_set = SSet2 }
             end
      end.
 
-x_field_args(Path, Args, SArgs) ->
-    [x_field_arg(Path, K, V, SArgs) || {K,V} <- Args].
+field_args(Path, Args, SArgs) ->
+    [field_arg(Path, K, V, SArgs) || {K,V} <- Args].
 
-x_field_arg(Path, K, V, SArgs) ->
+field_arg(Path, K, V, SArgs) ->
     N = name(K),
     case maps:get(N, SArgs, not_found) of
         not_found ->
@@ -135,7 +135,7 @@ field_arg_type(Ty) when is_binary(Ty) ->
         #input_object_type{} -> Ty
     end.
 
-x_field_lookup(Path, Ty, SSet) ->
+field_lookup(Path, Ty, SSet) ->
     case graphql_schema:lookup(Ty) of
         not_found ->
             graphql_err:abort(Path, {entity_not_found, Ty});
@@ -143,18 +143,18 @@ x_field_lookup(Path, Ty, SSet) ->
             graphql_err:abort(Path, fields_on_scalar);
         #scalar_type{} = Scalar ->
             %% Written like this for coherence, we could skip it since SSet == []
-            {Scalar, x_sset(Path, SSet, #{})};
+            {Scalar, sset(Path, SSet, #{})};
         #object_type{} when SSet == [] ->
             graphql_err:abort(Path, fieldless_object);
         #object_type{ fields = Fields } = Obj ->
-            {Obj, x_sset(Path, SSet, Fields)};
+            {Obj, sset(Path, SSet, Fields)};
         #interface_type{} when SSet == [] ->
             graphql_err:abort(Path, fieldless_interface);
         #interface_type{ fields = Fields } = IFace ->
-            {IFace, x_sset(Path, SSet, Fields)};
+            {IFace, sset(Path, SSet, Fields)};
         #union_type{} = Union ->
             %% Any field lookup on a union will fail as a result of this
-            {Union, x_sset(Path, SSet, #{})};
+            {Union, sset(Path, SSet, #{})};
         #enum_type{} = Enum when SSet == [] ->
             {Enum, []};
         #enum_type{} ->
