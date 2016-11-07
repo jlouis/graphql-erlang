9e46d2b7ae159de95b867ea02569481df51ebf39
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2016-11-07 13:13:13 +0100

    Simplify collect_fields/6
    We can simplify a lot of the internals in the collect_fields/6 function by moving
    more stuff to the top level pattern match and reuse paths through recursion.
    
    If we get a named fragment, we can just look it up and then tail-call into
    ourselves as if it was an anonymous fragment. This simplifies the code base
    and removes a code path which was the same anyway. While here, we can move
    more stuff to the top-level pattern match.

--- src/graphql_execute.erl
+++ src/graphql_execute.erl
@@ -84,39 +84,34 @@ collect_fields(Path, Ctx, Type, SSet, Visited) -> collect_fields(Path, Ctx, Type
    
 collect_fields(_Path, _Ctx, _Type, [], _Visited, Grouped) ->
     Grouped;
-collect_fields(Path, #{ fragments := Frags } = Ctx, Type, [S|SS], Visited, Grouped) ->
+collect_fields(Path, Ctx, Type, [#field{} = S |SS], Visited, Grouped) ->
+    ResponseKey = alias(S),
+    Grouped2 = orddict:append(ResponseKey, S, Grouped),
+    collect_fields(Path, Ctx, Type, SS, Visited, Grouped2);
+collect_fields(Path, Ctx, Type, [#frag_spread { id = ID }|SS], Visited, Grouped) ->
+    #{ fragments := Frags } = Ctx,
+    Name = name(ID),
+    %% TODO: Lift this to a function by itself called collect_view_fragment...
+    case maps:is_key(Name, Visited) of
+        true ->
+            collect_fields(Path, Ctx, Type, SS, Visited, Grouped);
+        false ->
+            case maps:get(Name, Frags, not_found) of
+                 not_found ->
+                     collect_fields(Path, Ctx, Type, SS, Visited#{ Name => true }, Grouped);
+            #frag{} = F ->
+                 collect_fields(Path, Ctx, Type, [F | SS], Visited#{ Name => true }, Grouped)
+            end
+    end;
+collect_fields(Path, Ctx, Type, [S |SS], Visited, Grouped) ->
     case S of
-        #field {} = F ->
-            ResponseKey = alias(F),
-            Grouped2 = orddict:append(ResponseKey, S, Grouped),
-            collect_fields(Path, Ctx, Type, SS, Visited, Grouped2);
-        #frag_spread{ id = ID } ->
-            Name = name(ID),
-            %% TODO: Lift this to a function by itself called collect_view_fragment...
-            case maps:is_key(Name, Visited) of
-                true ->
-                    collect_fields(Path, Ctx, Type, SS, Visited, Grouped);
-                false ->
-                    case maps:get(Name, Frags, not_found) of
-                        not_found ->
-                            collect_fields(Path, Ctx, Type, SS, Visited#{ Name => true }, Grouped);
-                        #frag{ selection_set = FragmentSSet } = Fragment ->
-                            case does_fragment_type_apply(Type, Fragment) of
-                                false ->
-                                    collect_fields(Path, Ctx, Type, SS, Visited#{ Name => true }, Grouped);
-                                true ->
-                                    FragGrouped = collect_fields([Name|Path], Ctx, Type, FragmentSSet, Visited),
-                                    Grouped2 = collect_groups(FragGrouped, Grouped),
-                                    collect_fields(Path, Ctx, Type, SS, Visited#{ Name => true }, Grouped2)
-                            end
-                    end
-            end;
-        #frag{ selection_set = FragmentSSet } = Fragment ->
+        #frag{ id = FragID, selection_set = FragmentSSet } = Fragment ->
             case does_fragment_type_apply(Type, Fragment) of
                 false ->
                     collect_fields(Path, Ctx, Type, SS, Visited, Grouped);
                 true ->
-                    FragGrouped = collect_fields(['...' | Path], Ctx, Type, FragmentSSet, Visited),
+                    FragGrouped = 
+                        collect_fields([name(FragID) | Path], Ctx, Type, FragmentSSet, Visited),
                     Grouped2 = collect_groups(FragGrouped, Grouped),
                     collect_fields(Path, Ctx, Type, SS, Visited, Grouped2)
             end            
@@ -433,6 +428,7 @@ value_object(Ctx, FieldEnv, [{K, Val} | Rest]) ->
 
 name(N) when is_binary(N) -> N;
 name({name, _, N}) -> N;
+name('...') -> <<"...">>;
 name(#field { id = ID }) -> name(ID).
 
 alias(#field { alias = undefined, id = ID }) -> name(ID);
