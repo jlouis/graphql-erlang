8741521d29e0790fbe28309bfece0bf640334b5b
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2017-01-04 21:13:27 +0100

    Add a placeholder for module resolvers
    When we want to inject modules from GraphQL schema files, we need to
    supply the resolver modules via the mapping. To make ready for this,
    we need this resolution into the system.

--- src/graphql_schema.hrl
+++ src/graphql_schema.hrl
@@ -1,75 +1,79 @@
 %% Schema definitions
 
+-type mod() :: atom().
 -type schema_base_type() :: binary() | {scalar, string | int | bool | float | id}.
 -type schema_type() ::
-	  {non_null, schema_base_type()}
-	| {non_null, {list, schema_base_type()}}
-	| {list, schema_base_type()}
-	| schema_base_type().
+        {non_null, schema_base_type()}
+      | {non_null, {list, schema_base_type()}}
+      | {list, schema_base_type()}
+      | schema_base_type().
 
 -type resolver_args() :: #{ binary() => term() }.
 -type ctx() :: #{ atom() => term() }.
 -type resolver() :: fun ((ctx, term(), resolver_args()) -> term()).
 
 -record(enum_value, {
-    val :: binary(),
-    description :: binary(),
-    deprecation = undefined :: undefined | binary()
-}).
+          val :: binary(),
+          description :: binary(),
+          deprecation = undefined :: undefined | binary()
+         }).
 -type enum_value() :: #enum_value{}.
 
 -record(enum_type, {
-	id :: binary(),
-	description :: binary(),
-	repr = tagged :: tagged | atom | binary,
-	values :: #{ integer() => enum_value() }
-}).
+          id :: binary(),
+          description :: binary(),
+          repr = tagged :: tagged | atom | binary,
+          values :: #{ integer() => enum_value() }
+         }).
 -type enum_type() :: #enum_type{}.
 
 -record(interface_type, {
-	id :: binary(),
-	description :: binary(),
-	resolve_type :: fun ((any()) -> {ok, atom()} | {error, term()}),
-	fields :: #{ binary() => schema_field() }
-}).
+          id :: binary(),
+          description :: binary(),
+          resolve_module :: mod(),
+          resolve_type :: fun ((any()) -> {ok, atom()} | {error, term()}),
+          fields :: #{ binary() => schema_field() }
+         }).
 -type interface_type() :: #interface_type{}.
 
 -record(union_type, {
-	id :: binary(),
-	description :: binary(),
-	resolve_type :: fun ((any()) -> {ok, atom()} | {error, term()}),
-	types :: [binary()]
-}).
+          id :: binary(),
+          description :: binary(),
+          resolve_module :: mod(),
+          resolve_type :: fun ((any()) -> {ok, atom()} | {error, term()}),
+          types :: [binary()]
+         }).
 -type union_type() :: #union_type{}.
 
 -record(schema_arg, {
-	ty :: schema_type(),
-	default = null :: any(),
-	description :: binary()
-}).
+          ty :: schema_type(),
+          default = null :: any(),
+          description :: binary()
+         }).
 -type schema_arg() :: #schema_arg{}.
 
 -record(directive_type, {
           id :: binary(),
           args = #{} :: #{ binary() => schema_arg() }
-}).
+         }).
 -type directive_type() :: #directive_type{}.
 
 -record(schema_field, {
-	ty :: schema_type(),
-	description :: binary() | undefined,
-	resolve = undefined :: undefined | resolver(),
-	deprecation = undefined :: undefined | binary(),
-	args = #{} :: #{ binary() => schema_arg() }
-}).
+          ty :: schema_type(),
+          description :: binary() | undefined,
+          resolve = undefined :: undefined | resolver(),
+          deprecation = undefined :: undefined | binary(),
+          args = #{} :: #{ binary() => schema_arg() }
+         }).
 -type schema_field() :: #schema_field{}.
 
 -record(scalar_type, {
-	id :: binary(),
-	description :: binary(),
-	output_coerce = fun(X) -> {ok, X} end :: fun ((any()) -> {ok, any()} | {error, any()}),
-	input_coerce = fun(X) -> {ok, X} end :: fun((any()) -> {ok, any()} | {error, any()})
-}).
+          id :: binary(),
+          description :: binary(),
+          resolve_module :: mod(),
+          output_coerce = fun(X) -> {ok, X} end :: fun ((any()) -> {ok, any()} | {error, any()}),
+          input_coerce = fun(X) -> {ok, X} end :: fun((any()) -> {ok, any()} | {error, any()})
+         }).
 -type scalar_type() :: #scalar_type{}.
 
 -record(input_object_type, {
@@ -80,24 +84,24 @@
 -type input_object_type() :: #input_object_type{}.
 
 -record(object_type, {
-	id :: binary(),
-	description :: binary(),
-	fields = #{} :: #{ binary() => schema_field() },
-	interfaces = [] :: [binary()]
-}).
+          id :: binary(),
+          description :: binary(),
+          resolve_module :: mod(),
+          fields = #{} :: #{ binary() => schema_field() },
+          interfaces = [] :: [binary()]
+         }).
 -type object_type() :: #object_type{}.
 
-
 -record(root_schema, {
-	id = 'ROOT' :: atom(),
-	query :: binary(),
-	mutation = undefined :: undefined | binary(),
-	subscription = undefined :: undefined | binary(),
-	interfaces = [] :: [binary()]
-}).
+          id = 'ROOT' :: atom(),
+          query :: binary(),
+          mutation = undefined :: undefined | binary(),
+          subscription = undefined :: undefined | binary(),
+          interfaces = [] :: [binary()]
+         }).
 -type root_schema() :: #root_schema{}.
 
 -type schema_object() ::
-	  object_type() | interface_type() | scalar_type()
-	| input_object_type() | union_type() | enum_type()
-	| root_schema().
+        object_type() | interface_type() | scalar_type()
+      | input_object_type() | union_type() | enum_type()
+      | root_schema().
