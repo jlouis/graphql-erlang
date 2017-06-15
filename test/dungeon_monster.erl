e637b92061a211930e06c5be6c20d99ad4e31b1c
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2017-06-15 21:56:33 +0200

    Add more monster execution code

--- test/dungeon_monster.erl
+++ test/dungeon_monster.erl
@@ -2,3 +2,48 @@
 -include("dungeon.hrl").
 
 -export([execute/4]).
+
+execute(_Ctx, #monster { id = ID,
+                         name = Name,
+                         hitpoints = HP,
+                         inventory = Inventory,
+                         mood = Mood,
+                         plush_factor = PlushFactor,
+                         stats = Stats,
+                         properties = Properties,
+                         color = Color}, Field, Args) ->
+    case Field of
+        <<"id">> -> dungeon:wrap({monster, ID});
+        <<"name">> -> {ok, Name};
+        <<"color">> -> color(Color, Args);
+        <<"hitpoints">> -> {ok, HP};
+        <<"hp">> -> {ok, HP};
+        <<"inventory">> -> {ok, [dungeon:load(OID) || OID <- Inventory]};
+        <<"mood">> -> {ok, Mood};
+        <<"plusFactor">> -> {ok, PlushFactor};
+        <<"stats">> -> stats(Stats, Args);
+        <<"statsVariantOne">> -> stats(Stats);
+        <<"statsVariantTwo">> -> stats(Stats);
+        <<"statsVariantThree">> -> stats(Stats);
+        <<"properties">> ->
+            {ok, [{ok, P} || P <- Properties]}
+    end.
+
+color(Color, #{ <<"colorType">> := <<"rgb">> }) ->
+    {ok, Color};
+color(Color, #{ <<"colorType">> := <<"gray">> }) ->
+    {ok, gray(Color)}.
+
+gray(#{ r := R,
+        g := G,
+        b := B }) ->
+    V = 0.30*R + 0.59*G + 0.11*B,
+    #{ r => V, g => V, b => V }.
+
+stats(SS) ->
+    {ok, [{ok, S} || S <- SS]}.
+                     
+stats(SS, #{ <<"minAttack">> := Min }) ->
+    {ok, [{ok, S} || S <- SS,
+                     S#stats.attack >= Min]}.
+
