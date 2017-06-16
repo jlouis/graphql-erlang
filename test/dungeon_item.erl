0497b4da35c3b69bf01ce5d32ddf76f762cc7046
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2017-06-16 12:58:15 +0200

    Add a type resolver and item handling

---
+++ test/dungeon_item.erl
@@ -0,0 +1,26 @@
+-module(dungeon_item).
+-include("dungeon.hrl").
+
+-export([execute/4]).
+
+execute(_Ctx, #item { id = ID,
+                      name = Name,
+                      contents = Contents,
+                      description = Description } = Item, Field, _) ->
+    case Field of
+        <<"id">> -> {ok, dungeon:wrap({item, ID})};
+        <<"name">> -> {ok, Name};
+        <<"description">> -> {ok, Description};
+        <<"weight">> -> {ok, weight(Item)};
+        <<"weightSum">> -> {ok, weight_sum(Item)};
+        <<"contents">> ->
+            {ok, [dungeon:load(OID) || OID <- Contents]}
+    end.
+
+weight(#item { weight = W }) -> W;
+weight(_) -> 0.0.
+
+weight_sum(#item { weight = W, contents = C }) ->
+    W + lists:sum([dungeon:load(OID) || OID <- C]);
+weight_sum(_) -> 0.0.
+
