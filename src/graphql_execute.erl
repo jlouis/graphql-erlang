d291abbdb4d4bc82e486772bf94abd18446969b5
David Cao <david.c.h.cao@gmail.com>
2017-09-08 09:32:01 -0300

    Add [] as valid object value in default_resolver/3 fun

--- src/graphql_execute.erl
+++ src/graphql_execute.erl
@@ -368,7 +368,7 @@ field_closure(Path, #{ defer_target := Upstream } = Ctx,
                 end
         end,
     #work { items = [{Ref, Closure}]}.
-    
+
 
 resolve_field_value(Ctx, #object_type { id = OID, annotations = OAns} = ObjectType, Value, Name, FAns, Fun, Args) ->
     CtxAnnot = Ctx#{
@@ -597,7 +597,7 @@ list_closure(Upstream, Self, Missing, List, Done) ->
               };
         ({change_ref, From, To}) ->
             {Val, Removed} = maps:take(From, Missing),
-            #work { items = [{Self, 
+            #work { items = [{Self,
                               list_closure(Upstream, Self,
                                            Removed#{ To => Val },
                                            List,
@@ -666,7 +666,7 @@ not_null_closure(Upstream, Self, Path, Ref) ->
                result = Res
               }
     end.
-    
+
 complete_list_value_result([]) ->
     {[], []};
 complete_list_value_result([{error, Err}|Next]) ->
@@ -725,11 +725,16 @@ resolver_function(#object_type { resolve_module = undefined }, undefined) ->
 resolver_function(#object_type { resolve_module = M }, undefined) ->
     fun M:execute/4.
 
-default_resolver(_, none, _) -> {error, no_object};
-default_resolver(_, undefined, _) -> {error, undefined_object};
+default_resolver(_, none, _) ->
+    {error, no_object};
+default_resolver(_, undefined, _) ->
+    {error, undefined_object};
 default_resolver(_, null, _) ->
     %% A Null value is a valid object value
     {ok, null};
+default_resolver(_, [], _) ->
+    %% A empty list [] value is a valid object value
+    {ok, []};
 default_resolver(#{ field := Field}, Cur, _Args) ->
     case maps:get(Field, Cur, not_found) of
         {'$lazy', F} when is_function(F, 0) -> F();
@@ -893,10 +898,10 @@ defer_loop(#defer_state { req_id = Id } = State) ->
 
 %% Cancel a token
 cancel(_Token, []) -> ok;
-cancel(Token, [Pid|Pids]) -> 
+cancel(Token, [Pid|Pids]) ->
     Pid ! {graphql_cancel, Token},
     cancel(Token, Pids).
-    
+
 %% Process work
 defer_handle_work(#defer_state {work = WorkMap,
                                 canceled = Cancelled } = State,
@@ -947,7 +952,7 @@ defer_handle_work(#defer_state {work = WorkMap,
 
 defer_handle_cancel(State, []) -> State;
 defer_handle_cancel(#defer_state { work = WorkMap,
-                                   canceled = Cancelled } = State, [R|Rs]) -> 
+                                   canceled = Cancelled } = State, [R|Rs]) ->
     case maps:take(R, WorkMap) of
         error ->
             defer_handle_cancel(
@@ -968,7 +973,7 @@ defer_handle_cancel(#defer_state { work = WorkMap,
                       ToCancel ++ Rs)
             end
     end.
-            
+
 
 %% -- ERROR HANDLING --
 
