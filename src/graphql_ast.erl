90831e5f772db7a6724ccb9e5a2ef876a4794e65
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2017-10-23 15:02:13 +0200

    Dead code elimination pass in graphql_ast

--- src/graphql_ast.erl
+++ src/graphql_ast.erl
@@ -3,37 +3,7 @@
 -include("graphql_internal.hrl").
 -include("graphql_schema.hrl").
 
--export([resolve_type/1, unwrap_to_base_type/1]).
--export([unwrap_type/1]).
 -export([name/1, id/1, typename/1]).
-
--spec resolve_type(graphql_type()) -> graphql_base_type().
-resolve_type({non_null, Ty}) -> resolve_type(Ty);
-resolve_type({list, Ty}) -> {list, resolve_type(Ty)};
-resolve_type(B) when is_binary(B) -> B;
-resolve_type(#scalar_type{} = Ty) -> Ty;
-resolve_type(#enum_type{} = Ty) -> Ty;
-resolve_type(#input_object_type{} = Ty) -> Ty;
-resolve_type(#object_type{} = Ty) -> Ty;
-resolve_type(#interface_type{} = Ty) -> Ty;
-resolve_type(#union_type{} = Ty) -> Ty;
-resolve_type({name, _, N}) -> N.
-
--spec unwrap_to_base_type(graphql_type()) -> graphql_base_type().
-unwrap_to_base_type({name, _, N}) -> N;
-unwrap_to_base_type(#enum_type{} = Ty) -> Ty;
-unwrap_to_base_type(#input_object_type{} = Ty) -> Ty;
-unwrap_to_base_type(#scalar_type{} = Ty) -> Ty;
-unwrap_to_base_type(#object_type{} = Ty) -> Ty;
-unwrap_to_base_type(#interface_type{} = Ty) -> Ty;
-unwrap_to_base_type(#union_type{} = Ty) -> Ty;
-unwrap_to_base_type(Ty) when is_binary(Ty) -> Ty;
-unwrap_to_base_type({non_null, Ty}) -> unwrap_to_base_type(Ty);
-unwrap_to_base_type({list, Ty}) -> unwrap_to_base_type(Ty).
-
--spec unwrap_type(graphql_type()) -> graphql_base_type().
-unwrap_type(Ty) ->
-    unwrap_to_base_type(Ty).
 
 -spec name('ROOT' | name()) -> binary().
 name('ROOT') -> <<"ROOT">>;
