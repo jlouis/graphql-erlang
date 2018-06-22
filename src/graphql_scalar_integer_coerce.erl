1192f24cec69ff9a77abf89509e54ec065ab9a96
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2018-06-22 17:35:16 +0200

    Introduce scalar coercion code for Jun2018 specification
    First step towards coercing integers correctly according to the
    Jun2018 specification. Still needs tests.

--- src/graphql_scalar_integer_coerce.erl
+++ src/graphql_scalar_integer_coerce.erl
@@ -2,8 +2,28 @@
 
 -export([input/2, output/2]).
 
-input(_, X) ->
-    {ok, X}.
+-define(MAX_INT, (1 bsl 31)).
+-define(MIN_INT, (-(1 bsl 31) - 1)).
 
-output(<<"Int">>, I) when is_integer(I) -> {ok, I};
-output(_,_)                             -> {ok, null}.
+input(Ty, X) when is_float(X) ->
+    T = trunc(X),
+    if
+        T == X -> input(Ty, T);
+        true ->
+            {error, float_truncate_not_integer}
+    end;
+input(_, X) when is_integer(X), X > ?MAX_INT -> {error, not_int32_value};
+input(_, X) when is_integer(X), X < ?MIN_INT -> {error, not_int32_value};
+input(_, X) -> {ok, X}.
+
+output(<<"Int">>, I) when is_integer(I) ->
+    if
+        I > ?MAX_INT ->
+            {error, not_int32_value};
+        I < ?MIN_INT ->
+            {error, not_int32_value};
+        true ->
+            {ok, I}
+    end;
+output(_,_) ->
+    {ok, null}.
