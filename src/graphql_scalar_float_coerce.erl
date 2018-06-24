fcda5c98fa4cb5c70e91f809f9ab9b7baa931cf3
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2018-06-24 16:09:41 +0200

    Handle Float values according to the Jun2018 specification.

--- src/graphql_scalar_float_coerce.erl
+++ src/graphql_scalar_float_coerce.erl
@@ -2,9 +2,10 @@
 
 -export([input/2, output/2]).
 
-input(_, X) ->
-    {ok, X}.
+input(_, X) when is_float(X)   -> {ok, X};
+input(_, X) when is_integer(X) -> {ok, float(X)};
+input(_, _)                    -> {error, not_float_coercible}.
 
-output(<<"Float">>, F) when is_float(F)  -> {ok, F};
-output(<<"Float">>,I) when is_integer(I) -> {ok, float(I)};
-output(_,_)                              -> {ok, null}.
+output(<<"Float">>, F) when is_float(F)   -> {ok, F};
+output(<<"Float">>, I) when is_integer(I) -> {ok, float(I)};
+output(_,_)                               -> {error, not_float_coercible}.
