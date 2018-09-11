28b42dce00e5a1cacf129a94b15c4aa9e7a9cc2c
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2018-09-11 14:18:31 +0200

    Strengthen the output engine a bit

--- src/graphql_scalar_binary_coerce.erl
+++ src/graphql_scalar_binary_coerce.erl
@@ -10,6 +10,14 @@ input(_, X) -> {ok, X}.
 output(_,B) when is_binary(B) ->
     %% Standard case where the code provided a String value
     {ok, B};
+output(_, [{ok, _V}|_] = Lst) ->
+    %% Assume we have a list of output values we can turn into strings
+    %% Then collect them as a string. This happens when you have a
+    %% default value which is a list of enum values for instance
+    Res = [output(<<"String">>, S) || {ok, S} <- Lst],
+    {ok, iolist_to_binary(["[",
+                           lists:join(",", Res),
+                           "]"])};
 output(_,X) when is_list(X) ->
     %% Literal string values in erlang are lists, so treat the data
     %% as iodata() and output it
@@ -17,12 +25,6 @@ output(_,X) when is_list(X) ->
         Val -> {ok, Val}
     catch _:_ -> {error, not_coercible}
     end;
-output(_, false) ->
-    %% Boolean false, these do not provide information loss
-    {ok, <<"false">>};
-output(_, true)  ->
-    %% Boolean true
-    {ok, <<"true">>};
 output(_, A) when is_atom(A) ->
     %% Atoms can be output stringently
     {ok, atom_to_binary(A, utf8)};
