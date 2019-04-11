1c1b1c77b1f3ac77b935903dfc95e88dd78588ca
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2019-04-11 15:12:13 +0200

    Correct the type we have, for field arguments

--- src/graphql_internal.hrl
+++ src/graphql_internal.hrl
@@ -28,7 +28,9 @@
 
 -record(field, {
 	id :: graphql:name(),
-	args = [] :: [any()],
+	args = [] :: [#{ type := graphql_type(),
+                        value := value(),
+                        default := undefined | value() }],
 	directives = [] :: [any()],
 	selection_set = [] :: [any()],
 	alias = undefined :: undefined | graphql:name(),
